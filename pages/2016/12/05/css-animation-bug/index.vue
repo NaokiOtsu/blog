8272ed809234fb49c4b24aaff522429fe409683f
NaokiOtsu <n.otsu1029@gmail.com>
2019-05-03 09:07:54 +0900

    add redirect setting

---
+++ pages/2016/12/05/css-animation-bug/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+if (process.browser) {
+  console.log(111, location.href, location.href.match(/(\/)/g))
+}
+export default {}
+</script>
+
+<style></style>
