b89afdc9799a76113aa08540904ec97d5a947901
NaokiOtsu <n.otsu1029@gmail.com>
2019-05-06 11:48:46 +0900

    add redirect file

---
+++ pages/2016/12/05/css-animation-bug/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+if (process.browser) {
+  location.href = 'https://naokiotsu.github.io/blog/2016-12-05-css-animation-bug/'
+}
+export default {}
+</script>
+
+<style></style>
